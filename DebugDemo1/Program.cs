357270f598aa8d903a39686670a4bc2c82c4aaf0
MarlinShenoda <marlinadel2000@gmail.com>
2022-05-10 16:23:44 +0200

    RemoveSum()

--- DebugDemo1/Program.cs
+++ DebugDemo1/Program.cs
@@ -9,13 +9,8 @@ namespace DebugDemo1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int value = Sum(2, 3);
-            Console.WriteLine(value);   
-        }
-
-        private static int Sum(int v1, int v2)
-        {
-          return v1+ v2;
-        }
+        }    
     }
+
+    
 }
