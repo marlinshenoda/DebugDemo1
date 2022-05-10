55dd66354526cb49ad9cea73ed839a77ab61bc8b
Marli <Marli@LAPTOP-EQB3NGRP>
2022-05-10 15:50:09 +0200

    Add project files.

---
+++ DebugDemo1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+
+namespace DebugDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int value = Sum(2, 3);
+            Console.WriteLine(value);   
+        }
+
+        private static int Sum(int v1, int v2)
+        {
+          return v1+ v2;
+        }
+    }
+}
